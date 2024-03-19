8429fd048888a182a3d51771b277636eca0c1f7c
cballrun <carter.k.ball@gmail.com>
2024-03-18 23:28:14 -0400

    Sales service able to load all available sales for given link

--- app/services/sales_service.rb
+++ app/services/sales_service.rb
@@ -4,6 +4,11 @@ class SalesService
     def initialize(card_link)
         @card_link = card_link
         initialize_driver
+        navigate_to_card_page
+        click_view_more_data_button
+        while load_more_sales_button_visible? do
+            click_load_more_sales_button
+        end
     end
 
     private
@@ -14,4 +19,35 @@ class SalesService
         @driver = Selenium::WebDriver.for :chrome, options: @options
         @wait = Selenium::WebDriver::Wait.new(timeout: 10)
     end
+
+    def navigate_to_card_page
+        @driver.get @card_link
+    end
+
+    def click_view_more_data_button
+        view_more_data_button = @wait.until do
+            @driver.find_element(:css, "div.modal__activator")
+        end
+        view_more_data_button.click
+    end
+
+    def load_more_sales_button
+        load_more_sales_button = @driver.find_element(:css, "button.sales-history-snapshot__load-more")
+    end
+
+    def load_more_sales_button_visible?
+        sleep 2
+        @wait.until { load_more_sales_button.displayed? }
+    rescue Selenium::WebDriver::Error::TimeoutError #THIS IS BAD CODE I NEED TO FIX IT
+        false
+    end
+
+    def click_load_more_sales_button
+        load_more_sales_button = @wait.until do
+            @driver.find_element(:css, "button.sales-history-snapshot__load-more")
+        end
+        sleep 2
+        load_more_sales_button.click
+    end
+
 end
