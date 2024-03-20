45d991f52f2733d0fc775699eae67af5dce6ba4e
cballrun <carter.k.ball@gmail.com>
2024-03-20 12:44:34 -0400

    Updates to extract_tcgplayerid, add market_value field to Variant.new

--- app/facades/variants_facade.rb
+++ app/facades/variants_facade.rb
@@ -4,7 +4,7 @@ class VariantsFacade
         variants_service = VariantsService.new(search_term)
         variants_array = variants_service.scrape_variants
         variants_array.map do |variant|
-            Variant.new(url: variant.url, set: variant.set, name: variant.name, rarity: variant.rarity, listing_count: variant.count, low_price: variant.low, tcgplayerid: variant.tcgplayerid)
+            Variant.new(url: variant.url, set: variant.set, name: variant.name, rarity: variant.rarity, listing_count: variant.count, low_price: variant.low, market_price: variant.market, tcgplayerid: variant.tcgplayerid)
         end
     end
 
