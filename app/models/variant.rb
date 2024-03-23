b0c4dc5e3d7c4e0b3f80a0f8cebd1237da9400cb
cballrun <carter.k.ball@gmail.com>
2024-03-23 11:59:33 -0400

    Create and test find_variant_by_url Variant model method

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -5,4 +5,8 @@ class Variant < ApplicationRecord
     validates_presence_of :name
     validates_presence_of :url
     validates_presence_of :tcgplayerid
+
+    def self.find_variant_by_url(url)
+      Variant.find_by(url: url)
+    end
   end
