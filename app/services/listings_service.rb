9c1973959571f03a673e595dad7bbb4817ec28fa
cballrun <carter.k.ball@gmail.com>
2024-03-23 12:59:28 -0400

    ListingsService able to navigate to given URL and locate listings on the first page

--- app/services/listings_service.rb
+++ app/services/listings_service.rb
@@ -4,6 +4,8 @@ class ListingsService
     def initialize(url)
         @url = url
         initialize_driver
+        navigate_to_card_page
+        find_listings
     end
 
     private 
@@ -13,5 +15,16 @@ class ListingsService
         # options.add_argument("--headless")
         @driver = Selenium::WebDriver.for :chrome, options: @options
         @wait = Selenium::WebDriver::Wait.new(timeout: 10)
+    end
+
+    def navigate_to_card_page
+        @driver.get @url
+    end
+
+    def find_listings
+        sleep 2
+        @wait.until do
+            @driver.find_elements(:css, "section.listing-item.product-details__listings-results")
+        end
     end
 end
--- spec/services/listings_service_spec.rb
+++ spec/services/listings_service_spec.rb
@@ -1,13 +1,22 @@
 require "rails_helper"
 
 RSpec.describe ListingsService do
-    describe "initialization" do
+    # describe "initialization" do
         
-        it 'exists' do
+    #     it 'exists' do
+    #         url = "https://www.tcgplayer.com/product/106996/pokemon-base-set-shadowless-alakazam?page=1"
+    #         listings_service = ListingsService.new(url)
+
+    #         expect(listings_service).to be_a(ListingsService)
+    #     end
+    # end
+
+    describe "gettings listings" do
+        it 'gets an array of listings for a given url' do
             url = "https://www.tcgplayer.com/product/106996/pokemon-base-set-shadowless-alakazam?page=1"
             listings_service = ListingsService.new(url)
-
-            expect(listings_service).to be_a(ListingsService)
+            binding.pry
+            expect(listings_service).to be_a(Array)
         end
     end
 end
